02cd31f2286533ccaffd0faa6f22c089de8d10bf
Gloria Uzoamaka Anidiobi <guzoamaka@yahoo.com>
2024-09-06 12:54:34 -0700

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,7 @@
+function signup(){
+    console.log("button clicked")
+    errorParagraph.textContent="Page isn't available at the moment, please try again"
+}
+
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph)
